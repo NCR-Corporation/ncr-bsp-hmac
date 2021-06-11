f41c8da93400be242dbcc8eb802f9a4e5baad383
Kevin Guebert <73493569+kg185155@users.noreply.github.com>
2021-06-11 09:50:31 -0400

    Update Example-Get.ps1

--- powershell/Example-Get.ps1
+++ powershell/Example-Get.ps1
@@ -11,8 +11,7 @@ if ($env:SHARED_KEY -And  $env:SECRET_KEY -And $env:ORGANIZATION) {
 }
 
 $url = "https://api.ncr.com/security/roles?roleNamePattern=*&pageNumber=0&pageSize=10"
-$now = Get-Date
-echo $now
+$now = Get-Date -UFormat "%A, %B %e, %Y %r"
 
 $accessKey = Get-AccessKey -sharedKey $sharedKey `
     -secretKey $secretKey `
