6b9b1cd81b35bbcddee8b23966bfc3fa22700c77
Kevin Guebert <73493569+kg185155@users.noreply.github.com>
2021-06-11 09:50:40 -0400

    Update Example-Post.ps1

--- powershell/Example-Post.ps1
+++ powershell/Example-Post.ps1
@@ -11,7 +11,7 @@ if ($env:SHARED_KEY -And  $env:SECRET_KEY -And $env:ORGANIZATION) {
 }
 
 $url = "https://api.ncr.com/security/authorization"
-$now = Get-Date
+$now = Get-Date -UFormat "%A, %B %e, %Y %r"
 
 $accessKey = Get-AccessKey -sharedKey $sharedKey `
     -secretKey $secretKey `
