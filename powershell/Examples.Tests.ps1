30210fc499403570d75d669baed672cfbcdac871
Kevin Guebert <kevin.guebert@ncr.com>
2021-06-11 09:14:27 -0400

    Adding Powershell Tests

---
+++ powershell/Examples.Tests.ps1
@@ -0,0 +1,14 @@
+Describe "API validation" {
+    BeforeAll {
+        $getResponse = .\Example-Get
+        $postResponse = .\Example-Post
+    }
+
+    It "GET response status code = '200'" {
+        $getResponse.statusCode | Should -Be '200'
+    }
+
+    It "POST response status code = '200'" {
+        $postResponse.statusCode | Should -Be '200'
+    }
+}
